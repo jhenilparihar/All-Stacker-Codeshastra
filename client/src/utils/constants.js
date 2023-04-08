325e9f417ab2313fadb1b02342d7d27a2f56a531
Jhenil Parihar <jhenilparihar@gmail.com>
2023-04-09 01:17:01 +0530

    commit

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./CafeSepolia.json";
 
 export const contractABI = abi.abi;
-export const contractAddress = "0x4Ae603F6c017C99FF32a85daAe5D2774E947408D";
+export const contractAddress = "0xe404424eF898B29E380f7918E4A49115d73d93B5";
